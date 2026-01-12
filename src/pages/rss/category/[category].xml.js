57303c23437fcc815d9599d8f308b834ae28c3b9
PremierofPotatoes <tbk98334@gmail.com>
2026-01-12 22:45:22 +0800

    additional changes

--- src/pages/rss/category/[category].xml.js
+++ src/pages/rss/category/[category].xml.js
@@ -1,6 +1,6 @@
 import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
-import { slugifyCategory } from '../../../../src/data/categories';
+import { slugifyCategory } from '../../../src/assets/PDF/Resume 02.pdf';
 
 export async function getStaticPaths() {
   const posts = await getCollection('blog');
