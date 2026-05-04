51e4d5ecbb466dbf168896c4dcd5a80b44a73a65
bladeacer <wg.nick.exe@gmail.com>
2026-05-04 15:52:34 +0800

    Update resume

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,18 +1,18 @@
 export const projects = [
 	{
+		title: "Resume",
+		techStack: "",
+		description: "My resume",
+		ctaText: "Download Resume →",
+		ctaLink: "/Portfolio/resume.pdf",
+		icon: "file-text"
+	},
+	{
 		title: "Portfolio",
 		techStack: "",
 		description: "My RSS feed",
 		ctaText: "View RSS feed →",
 		ctaLink: "/Portfolio/rss.xml",
 		icon: "rss"
-	},
-	{
-		title: "Cover letter",
-		techStack: "",
-		description: "My cover letter",
-		ctaText: "Download Cover Letter →",
-		ctaLink: "/Portfolio/resume.pdf",
-		icon: "file-user"
 	},
 ];
